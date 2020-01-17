405442e4ca505e9ae98a83775427e9cf992915ce
danieldamianov <danieldamianov02@gmail.com>
2020-01-18 01:10:36 +0200

    Initial Create

---
+++ src/Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(724, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 38);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "O - Wins :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(724, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 38);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "X - Wins :";
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Gold;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(731, 407);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(187, 57);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Start new game";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(101, 71);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(593, 581);
+            this.flowLayoutPanel1.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(892, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(892, 136);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(811, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Draws";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(892, 171);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 703);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ src/Tic Tac Toe/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Tic_Tac_Toe.Properties;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        TictactoeField tictactoeField00;
+        TictactoeField tictactoeField01;
+        TictactoeField tictactoeField02;
+        TictactoeField tictactoeField10;
+        TictactoeField tictactoeField11;
+        TictactoeField tictactoeField12;
+        TictactoeField tictactoeField20;
+        TictactoeField tictactoeField21;
+        TictactoeField tictactoeField22;
+        public Form1()
+        {
+            InitializeComponent();
+            tictactoeField00 = new TictactoeField();
+            tictactoeField01 = new TictactoeField();
+            tictactoeField02 = new TictactoeField();
+            tictactoeField10 = new TictactoeField();
+            tictactoeField11 = new TictactoeField();
+            tictactoeField12 = new TictactoeField();
+            tictactoeField20 = new TictactoeField();
+            tictactoeField21 = new TictactoeField();
+            tictactoeField22 = new TictactoeField();
+
+            flowLayoutPanel1.Controls.Add(tictactoeField00);
+            flowLayoutPanel1.Controls.Add(tictactoeField01);
+            flowLayoutPanel1.Controls.Add(tictactoeField02);
+            flowLayoutPanel1.Controls.Add(tictactoeField10);
+            flowLayoutPanel1.Controls.Add(tictactoeField11);
+            flowLayoutPanel1.Controls.Add(tictactoeField12);
+            flowLayoutPanel1.Controls.Add(tictactoeField20);
+            flowLayoutPanel1.Controls.Add(tictactoeField21);
+            flowLayoutPanel1.Controls.Add(tictactoeField22);
+
+            tictactoeField00.Size = new Size(130, 130);
+            tictactoeField01.Size = new Size(130, 130);
+            tictactoeField02.Size = new Size(130, 130);
+            tictactoeField10.Size = new Size(130, 130);
+            tictactoeField11.Size = new Size(130, 130);
+            tictactoeField12.Size = new Size(130, 130);
+            tictactoeField20.Size = new Size(130, 130);
+            tictactoeField21.Size = new Size(130, 130);
+            tictactoeField22.Size = new Size(130, 130);
+
+            tictactoeField00.Click += PositionChosedClick;
+            tictactoeField01.Click += PositionChosedClick;
+            tictactoeField02.Click += PositionChosedClick;
+            tictactoeField10.Click += PositionChosedClick;
+            tictactoeField11.Click += PositionChosedClick;
+            tictactoeField12.Click += PositionChosedClick;
+            tictactoeField20.Click += PositionChosedClick;
+            tictactoeField21.Click += PositionChosedClick;
+            tictactoeField22.Click += PositionChosedClick;
+
+            this.CurrentPlayer = TypeOfBox.Cross;
+        }
+
+        public void PositionChosedClick(object sender, EventArgs e)
+        {
+            TictactoeField buttonPressed = (TictactoeField)sender;
+            if (buttonPressed.elementes != null)
+            {
+                return;
+            }
+            turns++;
+            buttonPressed.elementes = this.CurrentPlayer;
+            if (this.CurrentPlayer == TypeOfBox.Cross)
+            {
+                buttonPressed.BackgroundImage = new Bitmap(Resources.mark, new Size(130, 130));
+                this.CurrentPlayer = TypeOfBox.Circle;
+            }
+            else
+            {
+                buttonPressed.BackgroundImage = new Bitmap(Resources.circle, new Size(130, 130));
+                this.CurrentPlayer = TypeOfBox.Cross;
+            }
+
+            if (this.CheckWiner())
+            {
+                if (this.CurrentPlayer == TypeOfBox.Circle)
+                {
+                    DisableButtons();
+                    MessageBox.Show("Winner cross");
+                    this.PlayerXWins++;
+                    this.textBox2.Text = this.PlayerXWins.ToString();
+                    this.turns = 0;
+                    return;
+                }
+                DisableButtons();
+                this.PlayerOWins++;
+                this.textBox1.Text = this.PlayerOWins.ToString();
+                this.turns = 0;
+                MessageBox.Show("Winner circle");
+            }
+            else if (turns == 9)
+            {
+                DisableButtons();
+                MessageBox.Show("Draw");
+                Draws++;
+                this.turns = 0;
+                this.textBox3.Text = this.Draws.ToString();
+            }
+        }
+
+        public void DisableButtons()
+        {
+            tictactoeField00.Enabled = false;
+            tictactoeField01.Enabled = false;
+            tictactoeField02.Enabled = false;
+            tictactoeField10.Enabled = false;
+            tictactoeField11.Enabled = false;
+            tictactoeField12.Enabled = false;
+            tictactoeField20.Enabled = false;
+            tictactoeField21.Enabled = false;
+            tictactoeField22.Enabled = false;
+        }
+
+        public TypeOfBox CurrentPlayer { get; set; }
+
+        public int PlayerXWins = 0;
+        public int PlayerOWins = 0;
+        public int Draws = 0;
+        public int turns = 0;
+        
+        private bool CheckWiner()
+        {
+            if ((this.tictactoeField00.elementes == this.tictactoeField01.elementes)
+                && (this.tictactoeField02.elementes == tictactoeField01.elementes)
+                && this.tictactoeField00.elementes != null)
+            {
+                return true;
+            }
+
+            if ((this.tictactoeField10.elementes == this.tictactoeField11.elementes)
+                && (this.tictactoeField12.elementes == tictactoeField11.elementes)
+                && this.tictactoeField10.elementes != null)
+            {
+                return true;
+            }
+
+            if ((this.tictactoeField20.elementes == this.tictactoeField21.elementes)
+                && (this.tictactoeField22.elementes == tictactoeField21.elementes)
+                && this.tictactoeField20.elementes != null)
+            {
+                return true;
+            }
+
+            if ((this.tictactoeField00.elementes == this.tictactoeField10.elementes)
+                && (this.tictactoeField20.elementes == tictactoeField10.elementes)
+                && this.tictactoeField20.elementes != null)
+            {
+                return true;
+            }
+
+            if ((this.tictactoeField01.elementes == this.tictactoeField11.elementes)
+                && (this.tictactoeField21.elementes == tictactoeField11.elementes)
+                && this.tictactoeField01.elementes != null)
+            {
+                return true;
+            }
+
+            if ((this.tictactoeField02.elementes == this.tictactoeField12.elementes)
+                && (this.tictactoeField22.elementes == tictactoeField12.elementes)
+                && this.tictactoeField02.elementes != null)
+            {
+                return true;
+            }
+
+            if ((this.tictactoeField00.elementes == this.tictactoeField11.elementes)
+                && (this.tictactoeField22.elementes == tictactoeField11.elementes)
+                && this.tictactoeField00.elementes != null)
+            {
+                return true;
+            }
+
+            if ((this.tictactoeField20.elementes == this.tictactoeField11.elementes)
+                && (this.tictactoeField02.elementes == tictactoeField11.elementes)
+                && this.tictactoeField20.elementes != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public enum TypeOfBox
+        {
+            Cross,
+            Circle
+        }
+        public class TictactoeField : Button
+        {
+            public TypeOfBox? elementes { get; set; }
+
+            public TictactoeField()
+            {
+                elementes = null;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            tictactoeField00.Enabled = true;
+            tictactoeField01.Enabled = true;
+            tictactoeField02.Enabled = true;
+            tictactoeField10.Enabled = true;
+            tictactoeField11.Enabled = true;
+            tictactoeField12.Enabled = true;
+            tictactoeField20.Enabled = true;
+            tictactoeField21.Enabled = true;
+            tictactoeField22.Enabled = true;
+
+            tictactoeField00.BackgroundImage = null;
+            tictactoeField01.BackgroundImage = null;
+            tictactoeField02.BackgroundImage = null;
+            tictactoeField10.BackgroundImage = null;
+            tictactoeField11.BackgroundImage = null;
+            tictactoeField12.BackgroundImage = null;
+            tictactoeField20.BackgroundImage = null;
+            tictactoeField21.BackgroundImage = null;
+            tictactoeField22.BackgroundImage = null;
+
+            tictactoeField00.elementes = null;
+            tictactoeField01.elementes = null;
+            tictactoeField02.elementes = null;
+            tictactoeField10.elementes = null;
+            tictactoeField11.elementes = null;
+            tictactoeField12.elementes = null;
+            tictactoeField20.elementes = null;
+            tictactoeField21.elementes = null;
+            tictactoeField22.elementes = null;
+        }
+    }
+}
